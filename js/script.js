3891e8e9bc2989da315226c812c17c3525e1f39e
Robert <rspatz@live.com>
2024-04-08 14:37:09 -0500

    Create slideshow

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Carousel
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// Forward/Back Controls
+function plusSlides(n) {
+  SlideShow((slidePosition += n));
+}
+
+//  Image Controls
+function currentSlide(n) {
+  SlideShow((slidePosition = n));
+}
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName('family-container');
+
+  if (n > slides.length) {
+    slidePosition = 1;
+  }
+
+  if (n < 1) {
+    slidePosition = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  slides[slidePosition - 1].style.display = 'flex';
+}
